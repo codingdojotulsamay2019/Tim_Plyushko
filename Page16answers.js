46ef194c535ada7ad52a380a98bad796005965a6
timadvancing <50765883+timadvancing@users.noreply.github.com>
2019-05-30 17:21:37 -0500

    Add files via upload
    Page 16 Answers Work

---
+++ Page16answers.js
@@ -0,0 +1,86 @@
+Page 16 of Algorithm Book Pre-Bootcamp
+
+Setting and Swapping
+
+var myNumber = 42;
+var myName = Tim;
+var myNumber = var myName
+var myName = myNumber
+
+Print -52 to 1066
+
+function print-52To1066 (){
+    var arr[];
+    for(var i = -52; i < 1067; i++){
+        arr.push(i);
+    }
+    return arr;
+}
+
+Don't Worry, Be Happy
+
+function beCheerful(){
+    for(var i = 1; i < 99; i++){
+        console.log(beCheerful);
+    }
+}
+
+Multiples of Three - but Not All
+
+function getmultiplesof3();
+    for(var i = -300; i < 1; i++){
+        if (i == -6)
+        {
+            continue;
+        }
+        if (i == -3)
+        {
+            continue;        }
+        }
+        console.log(getmultipesof3);
+    }
+
+Printing Integers with While 
+
+function printint();
+    while(var i = 2000; i < 5280; i++){
+        console.log(i);
+}
+
+You Say It's Your Birthday
+
+function birthmonthandday();
+    if month and day in either order{
+        console.log("How did you know?");
+    }
+    else {
+        console.log ("Just another day . . .")
+    }
+
+Leap Year
+
+function leapyear();
+    if year divisible by 4{
+        if not divisible by 100{
+            if divisible by 400{
+                console.log(leapyear);
+            }
+            else{
+            console.log(notleapyear);
+            }
+        else{
+            console.log(notleapyear);
+        }
+    else{
+        console.log(notleapyear);
+    }
+}
+
+Print and Count
+
+function printmultiples();
+    for(var i = 512; i < 4097; i++){
+        i += 5;
+        arr.push(i);
+        console.log(arr[]);
+        }
